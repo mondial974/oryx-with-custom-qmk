e27f0a597adf79bb1bce2dfe967f79261a585756
Jérôme Fontaine <j.fontaine@infrabat-ingenierie.fr>
2025-10-17 00:36:27 +0400

    del uniquement

--- Oze63/keymap.c
+++ Oze63/keymap.c
@@ -280,6 +280,6 @@ const key_override_t delete_key_override =
     ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 const key_override_t *key_overrides[] = {
-	&delete_key_override
+	&delete_key_override,
   NULL
 };
